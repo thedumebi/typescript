5f711949a508ca6c62d9fdba22ce85a47706e6ba
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2021-11-11 13:29:16 +0100

    another generic function

--- Generics/dist/app.js
+++ Generics/dist/app.js
@@ -4,4 +4,15 @@ function merge(objA, objB) {
 }
 var mergedObj = merge({ name: "DMB", hobbies: ["Andrea"] }, { age: 23 });
 console.log(mergedObj);
+function countAndDescribe(element) {
+    var descriptionText = "Got no value.";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element.";
+    }
+    else if (element.length > 0) {
+        descriptionText = "Got " + element.length + " elements.";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(["Sports", "Cooking"]));
 //# sourceMappingURL=app.js.map
--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -17,3 +17,19 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: "DMB", hobbies: ["Andrea"] }, { age: 23 });
 console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 0) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
