0b2ad9c84a03bee959d0832e49bbe3908ada13a2
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2021-11-13 13:21:22 +0100

    adding multiple decorators

--- Decorators/dist/app.js
+++ Decorators/dist/app.js
@@ -6,13 +6,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 function Logger(logString) {
+    console.log("LOGGER FACTORY");
     return function (constructor) {
         console.log(logString);
         console.log(constructor);
     };
 }
 function WithTemplate(template, hookId) {
+    console.log("TEMPLATE FACTORY");
     return function (constructor) {
+        console.log("Rendering template");
         const hookEl = document.getElementById(hookId);
         const p = new constructor();
         if (hookEl) {
@@ -28,6 +31,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
+    Logger("LOGGING"),
     WithTemplate("<h1>My Person Object</h1>", "app")
 ], Person);
 const pers = new Person();
