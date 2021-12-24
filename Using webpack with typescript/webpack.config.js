1aafd24b4a694174108c742c87427e5241abf195
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2021-12-24 13:20:39 +0100

    finishing setup & adding webpack-dev-server

--- Using webpack with typescript/webpack.config.js
+++ Using webpack with typescript/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
+  },
+  devServer: {
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, "/"),
+    },
   },
   devtool: "inline-source-map",
   module: {
