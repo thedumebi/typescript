6f3b44e43ec46cc891c6c9a749e6f1b23f5a11b1
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2021-12-24 12:34:19 +0100

    adding entry and output configuration

--- Using webpack with typescript/src/components/project-list.ts
+++ Using webpack with typescript/src/components/project-list.ts
@@ -1,9 +1,9 @@
-import Component from "./base-component.js";
-import { ProjectItem } from "./project-item.js";
-import { DragTarget } from "../models/drag-drop.js";
-import { autobind } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
-import { ProjectStatus, Project } from "../models/project.js";
+import Component from "./base-component";
+import { ProjectItem } from "./project-item";
+import { DragTarget } from "../models/drag-drop";
+import { autobind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
+import { ProjectStatus, Project } from "../models/project";
 
 // ProjectList Class
 export class ProjectList
