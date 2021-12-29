3ec3305e5cdde31862c594158398de1a1cb3460c
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2021-12-29 14:14:11 +0100

    using axios to fetch coordinates for an entered address

--- Select And Share A Place App/src/app.ts
+++ Select And Share A Place App/src/app.ts
@@ -1,11 +1,39 @@
+import axios from "axios";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "AIzaSyBJa50jqbsetkdq_3gvjWgH23at-KkXGOk";
 
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      addressInput.value = "";
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      console.log(response.data.results);
+
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
