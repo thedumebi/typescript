ef520b6ec77b1851dca471fa80f47ee4ce99b085
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2021-12-30 14:10:22 +0100

    types for other react features (e.g. redux or routing)

--- react-and-typescript/src/App.tsx
+++ react-and-typescript/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+// import { Route } from "react-router-dom";
 import NewTodo from "./components/NewTodo";
 import TodoList from "./components/TodoList";
 import { Todo } from "./todo.models";
