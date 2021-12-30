e6b850f04742547839ef0c360df06c1d753f04bf
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2021-12-30 14:00:02 +0100

    more prop and state work

--- react-and-typescript/src/components/TodoList.tsx
+++ react-and-typescript/src/components/TodoList.tsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { Todo } from "../todo.models";
 
 interface TodoListProps {
-  items: { id: string; text: string }[];
+  items: Todo[];
+  onDeleteTodo: (id: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
       ))}
     </ul>
   );
