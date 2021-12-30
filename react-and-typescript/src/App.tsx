b17cd1e002b33c2926aaf9f0b4035ab02b350fbc
Chiwuzoh Daniel <chiwuzohdumebi@gmail.com>
2021-12-30 13:18:06 +0100

    how do react + typescript work together

--- react-and-typescript/src/App.tsx
+++ react-and-typescript/src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>Hello World</h1>
+  </div>;
 };
 
 export default App;
